182c7b685adbf4d62cb4bac484b53755af353481
Doğa Bircan Deviren <d.bircandeviren@gmail.com>
2020-11-22 22:36:16 +0300

    Hole is created. (Not yet Gameobject-ifyed)

---
+++ ColorHole3DClone/Assets/Layering/LayerNames.cs
@@ -0,0 +1,7 @@
+﻿
+public static class LayerNames
+{
+    public static readonly string Hole = "Hole";
+    public static readonly string Passable = "Passable";
+    public static readonly string Unpassable = "Unpassable";
+}
---
+++ ColorHole3DClone/Assets/Passability/Passability.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Passability : MonoBehaviour
+{
+    [SerializeField]
+    private bool badPiece = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name + "  entered with " + gameObject.name);
+
+        if (LayerMask.LayerToName(other.gameObject.layer) == LayerNames.Hole)
+            gameObject.layer = LayerMask.NameToLayer(LayerNames.Passable);
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log(other.gameObject.name + "  exit from " + gameObject.name);
+
+        if (LayerMask.LayerToName(other.gameObject.layer) == LayerNames.Hole)
+            gameObject.layer = LayerMask.NameToLayer(LayerNames.Unpassable);
+    }
+}
